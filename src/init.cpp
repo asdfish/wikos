d67e5de79259db8fa14a3e35d6ec16cdc692f6c3
asdfish <lgray3420@gmail.com>
2024-10-16 13:37:12 -0400

    added utils

--- src/init.cpp
+++ src/init.cpp
@@ -1,7 +1,13 @@
+#include <files.hpp>
 #include <init.hpp>
+
 #include <string>
 #include <vector>
+#include <filesystem>
 
 int init(void) {
+  std::vector<std::string> directories;
+  directories.append(directory_directories("."));
+
   return 0;
 }
