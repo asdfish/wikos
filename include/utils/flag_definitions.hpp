551f4005b22d34c79f07e8bd16b460a46954242e
asdfish <lgray3420@gmail.com>
2024-10-17 19:51:37 -0400

    removed unused globals

--- include/utils/flag_definitions.hpp
+++ include/utils/flag_definitions.hpp
@@ -7,15 +7,6 @@
 #include <string>
 #include <vector>
 
-extern const std::vector<std::string> checks;
-extern const std::vector<std::string> mumerical_abstract_domains;
-extern const std::vector<std::string> optimization_levels;
-extern const std::vector<std::string> procedural_analysis;
-extern const std::vector<std::string> widening_strategies;
-extern const std::vector<std::string> narrowing_strategies;
-extern const std::vector<std::string> partitioning_type;
-extern const std::vector<std::string> others;
-
 extern std::vector<std::unique_ptr<FlagBase>> flags;
 
 extern void flags_define(void);
